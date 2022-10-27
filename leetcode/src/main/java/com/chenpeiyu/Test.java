efff44ae7cd2348b1795bc0886080d17a09d4210
chen peiyu <chenpeiyu2001@outlook.com>
2022-10-27 18:48:06 +0800

    记录平时leetCode刷题的

--- leetcode/src/main/java/com/chenpeiyu/Test.java
+++ leetcode/src/main/java/com/chenpeiyu/Test.java
@@ -5,12 +5,9 @@ import java.util.Stack;
 public class Test {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
-
         stack.push(1);
         stack.push(2);
         stack.push(3);
-
         System.out.println(stack.peek());
-
     }
 }
