497e2d7562e96333cff80992f6a8cfae72b7fcd4
chen peiyu <chenpeiyu2001@outlook.com>
2022-11-15 19:11:14 +0800

    记录平时leetCode刷题的

--- leetcode/src/main/java/com/chenpeiyu/Jianzhi27.java
+++ leetcode/src/main/java/com/chenpeiyu/Jianzhi27.java
@@ -5,15 +5,14 @@ public class Jianzhi27 {
         System.out.println("hello world");
     }
 
-    public static TreeNode mirrorTree(TreeNode root){
-            if (root == null) {
-                return null;
-            }
-            TreeNode leftRoot = mirrorTree(root.right);
-            TreeNode rightRoot = mirrorTree(root.left);
-            root.left = leftRoot;
-            root.right = rightRoot;
-            return root;
+    public static TreeNode mirrorTree(TreeNode root) {
+        if (root == null) {
+            return null;
         }
+        TreeNode leftRoot = mirrorTree(root.right);
+        TreeNode rightRoot = mirrorTree(root.left);
+        root.left = leftRoot;
+        root.right = rightRoot;
+        return root;
     }
 }
