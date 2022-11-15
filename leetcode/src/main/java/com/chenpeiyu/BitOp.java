14ce35dac9c314cd872ca395dcb5c5dfff42236d
chen peiyu <chenpeiyu2001@outlook.com>
2022-11-15 19:52:10 +0800

    记录平时leetCode刷题的

--- leetcode/src/main/java/com/chenpeiyu/BitOp.java
+++ leetcode/src/main/java/com/chenpeiyu/BitOp.java
@@ -2,18 +2,32 @@ package com.chenpeiyu;
 
 public class BitOp {
     public static void main(String[] args) {
-        int res = pow(3,13);
+        int res = pow(13, 3);
+        System.out.println(res);
+
+        System.out.println(findN(19));
+
     }
-    public static int pow(int n,int m){
+
+    public static int pow(int n, int m) {
         int sum = 1;
         int temp = m;
-        while (n!=0){
-            if ((n & 1)==1){
-                sum*=temp;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                sum *= temp;
             }
-            temp*=temp;
-            n=n >> 1;
+            temp *= temp;
+            n = n >> 1;
         }
         return sum;
+    }
+
+
+    public static int findN(int n) {
+        n |= n >> 1;
+        n |= n >> 2;
+        n |= n >> 4;
+        n |= n >> 8;// 整型一般是 32 位，上面我是假设 8 位。
+        return (n + 1) >> 1;
     }
 }
